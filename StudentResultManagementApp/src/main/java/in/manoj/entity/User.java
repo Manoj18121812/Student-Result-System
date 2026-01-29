72180ac2894925f86af73ea152822139ef50a5b9
Manoj Gavhane <131341403+Manoj18121812@users.noreply.github.com>
2026-01-29 14:56:03 +0530

    modified

--- StudentResultManagementApp/src/main/java/in/manoj/entity/User.java
+++ StudentResultManagementApp/src/main/java/in/manoj/entity/User.java
@@ -1,48 +1,21 @@
 package in.manoj.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
 @Data
-@Table(name="USERS")
+@Table(name = "USERS")
 public class User {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	
-	private String email;
-	
-	private String password;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	public Long getId() {
-		return id;
-	}
+    private String email;
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    private String password;
 
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	
-	
+    @Column(name = "is_active", nullable = false)
+    private Boolean isActive = true;   // ✅ DEFAULT VALUE
 }
